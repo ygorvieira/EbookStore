b201be009193e895a9b152b9d4b5ddbba3724d45
Ygor Vieira <ygorus@gmail.com>
2018-01-08 11:08:01 -0200

    Criação da ViewModel do Carrinho

--- EbookStore/Controllers/PedidoController.cs
+++ EbookStore/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
 ﻿using EbookStore.Models;
+using EbookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -30,6 +31,13 @@ namespace EbookStore.Controllers
 
         public IActionResult Carrinho()
         {
+            CarrinhoViewModel viewModel = GetCarrinhoViewModel();
+
+            return View(viewModel);
+        }
+
+        private CarrinhoViewModel GetCarrinhoViewModel()
+        {
             var itensCarrinho = new List<ItemPedido>
             {
                 new ItemPedido(1, produtos[0], 1),
@@ -37,12 +45,15 @@ namespace EbookStore.Controllers
                 new ItemPedido(3, produtos[2], 3)
             };
 
-            return View(itensCarrinho);
+            CarrinhoViewModel viewModel = new CarrinhoViewModel(itensCarrinho);
+            return viewModel;
         }
 
         public IActionResult Resumo()
         {
-            return View();
+            CarrinhoViewModel viewModel = GetCarrinhoViewModel();
+
+            return View(viewModel);
         }
     }
 }
